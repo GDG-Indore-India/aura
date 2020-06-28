b93d0a1c76f903aa52ff9f56c937232e24e20af7
Priyal Jain <priyaljain919@gmail.com>
2020-06-29 02:41:14 +0530

    code updated

--- src/components/contact/ContactDetails.vue
+++ src/components/contact/ContactDetails.vue
@@ -4,7 +4,9 @@
             <v-col md="9" sm="6" cols="12" class="py-0 my-0">
                 <p class="google-font mb-1" style="font-size:110%">Our events are open to newbies, developers, managers, and organizations who are interested in Google's technologies or use them as part of their projects.</p>
                 <p class="google-font">{{data.shortDescription}}</p>
-                <p class="google-font mt-5">Questions? Please contact {{data.email}}</p>
+                <p class="google-font mt-5">Questions? Please contact 
+                    <a href="mailto:gdgindore@gmail.com" target="_blank">{{data.email}}</a>
+                    </p>
                 <span v-for="(item,i) in data.hashtags" :key="i">
                     <v-chip class="mr-1 chip-wrapper" :href="'https://twitter.com/hashtag/'+item" target="_blank" label>#{{item}}</v-chip>
                 </span>
